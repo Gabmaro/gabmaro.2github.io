337d8795627d7f75f6da2c79a2c6d70e85a2d108
Gabmaro <93953401+Gabmaro@users.noreply.github.com>
2021-11-11 02:52:48 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyDOYMpp68_udyaxhGI6xbtyF0n2HURHnP4",
-  authDomain: "fbauten1001.firebaseapp.com",
-  projectId: "fbauten1001",
-  storageBucket: "fbauten1001.appspot.com",
-  messagingSenderId: "717078882003",
-  appId: "1:717078882003:web:c4c3e1e5a917385cced626",
-  measurementId: "G-3QL4D2NXDN"
+   apiKey: "AIzaSyBNB3Pj6tYCE3SglpTZacSyWRs6CMrkNoM",
+  authDomain: "gabautent299.firebaseapp.com",
+  projectId: "gabautent299",
+  storageBucket: "gabautent299.appspot.com",
+  messagingSenderId: "768003291233",
+  appId: "1:768003291233:web:501d3f3a6b89c392fc9220",
+  measurementId: "G-NTVL5PT5GV"
     });
